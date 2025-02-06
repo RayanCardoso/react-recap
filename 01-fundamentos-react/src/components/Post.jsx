0306d64711cfea1088115760fe347ee3ce3ff382
RayanCardoso <rayancardoso40@gmail.com>
2025-02-05 21:12:13 -0300

    Updates before using TypeScript

--- 01-fundamentos-react/src/components/Post.jsx
+++ 01-fundamentos-react/src/components/Post.jsx
@@ -34,7 +34,12 @@ export function Post({author, publishedAt, content}) {
     }
     
     function handleNewCommentChange() {
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Este campo é obrigatório!");
     }
     
     function deleteComment(commentToDelete){
@@ -42,6 +47,8 @@ export function Post({author, publishedAt, content}) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article className={styles.post}>
@@ -76,11 +83,17 @@ export function Post({author, publishedAt, content}) {
                     value={newCommentText}
                     placeholder="Deixe um comentário"
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
                     required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button 
+                        type="submit"
+                        disabled={isNewCommentEmpty}
+                    >
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
