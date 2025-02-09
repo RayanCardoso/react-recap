eb313720110d86fc8054cc9df29559e36f7618ed
RayanCardoso <rayancardoso40@gmail.com>
2025-02-08 23:41:17 -0300

    finishing history page interface and inserting styled components inside table

---
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -0,0 +1,64 @@
+import { HistoryContainer, HistoryList, Status } from "./style";
+
+export function History() {
+    return (
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>                        
+                        <tr>
+                            <td>Tarefa 3</td>
+                            <td>20 min</td>
+                            <td>Há cerca de 1 dia</td>
+                            <td>
+                                <Status statusColor="yellow">
+                                    Em andamento
+                                </Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa 1</td>
+                            <td>40 min</td>
+                            <td>Há cerca de 2 dias</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluído
+                                </Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa 4</td>
+                            <td>40 min</td>
+                            <td>Há cerca de 3 dias</td>
+                            <td>
+                                <Status statusColor="red">
+                                    Interrompido
+                                </Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa 2</td>
+                            <td>60 min</td>
+                            <td>Há cerca de 1 mês</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluído
+                                </Status>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+    )
+}
